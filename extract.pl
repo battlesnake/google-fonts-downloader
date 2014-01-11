91befbe018bb608c1f2e38d8f0b1b86589676094
mark <mark@battlesnake.co.uk>
2014-01-11 03:38:52 +0000

    Minor tweaks

--- extract.pl
+++ extract.pl
@@ -3,13 +3,15 @@
 use LWP::Simple;
 
 my $face, $style, $weight, $name1, $name2, $format, $url;
+my $block = 0;
 
 print "face\tstyle\tweight\tname\tsname\tformat\turl\n";
 while (<>) {
-	if (/^@font-face\ \{$/) {
+	if (/^\@font-face\ \{$/) {
 		$face = $style = $weight = $name1 = $name2 = $url = $format = "";
+		$block = 1;
 	}
-	elsif (/font-family: '(\w+)';/) {
+	elsif (/font-family: '([\w\ ]+)';/) {
 		$face = $1;
 	}
 	elsif (/font-style: (\w+);/) {
@@ -25,8 +27,15 @@ while (<>) {
 		$format = $4;
 	}
 	elsif (/^\}$/) {
+		if (!$block) {
+			die "Data without block!";
+		}
+		$block = 0;
 		print "$face\t$style\t$weight\t$name1\t$name2\t$format\t$url\n";
-		my $path = lc("$face");
+		my $dname = $face;
+		$dname =~ s/\W//g;
+		$dname = lc($dname);
+		my $path = $dname;
 		my $fname = "$path/$style-$weight.$format";
 		mkdir("$path");
 		my $status = getstore($url, $fname);
